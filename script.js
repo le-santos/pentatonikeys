20f4d8a5cb98cb6a2bab0a6ba441666d5af9a66c
le-santos <lequinterio@gmail.com>
2020-05-05 20:38:05 -0300

    Modificação dos touch event e functions

--- script.js
+++ script.js
@@ -1,60 +1,53 @@
 "use strict"
 
-/* TO DO:
-		+ Setup de event para mudar cores do background 
-		+ Tornar responsive para web mobile 
-*/
-
-
-const makeSound = function(e) {
+const makeSound = function (e) {
 	console.log(e);
-	if (e.type === 'keypress'){
+	if (e.type === 'keypress') {
 		let sound = document.querySelector(`audio[data-key="${e.key}"]`);
-		if(!sound) return;
+		if (!sound) return;
 		sound.currentTime = 0;
 		sound.play();
 
-	} else if (e.type === 'touchstart' || e.type === 'touchmove'  ) {
+	} else if (e.type === 'touchstart') {
 		let sound = document.querySelector(`audio[data-key="${e.originalTarget.dataset.key}"]`);
-		if(!sound) return;
+		if (!sound) return;
 		sound.currentTime = 0;
 		sound.play();
 	}
 
 }
 
-const moveButton = function(e) {
+const moveButton = function (e) {
 	let key
 	console.log(e.type)
 
-	if (e.type === "keypress" ){
+	if (e.type === "keypress") {
 		key = document.querySelector(`button[data-key="${e.key}"]`);
-	} else if (e.type === "touchstart"){
+	} else if (e.type === "touchstart") {
 		key = document.querySelector(`button[data-key="${e.originalTarget.dataset.key}"]`);
 	}
 
-	if (key.className === 'chord chord-playing' || key.className === 'note note-playing'){
+	if (key.className === 'chord chord-playing' || key.className === 'note note-playing') {
 		return
-	} else if (key.className === 'chord'){
+	} else if (key.className === 'chord') {
 		key.classList.add('chord-playing');
 	} else {
 		key.classList.add('note-playing');
 	}
 }
 
-const removeTransition = function(e) {
+const removeTransition = function (e) {
 
-	if (e.propertyName !== 'transform' ){
+	if (e.propertyName !== 'transform') {
 		return;
 
-	} else if (e.target.classList[1] === 'note-playing'){
+	} else if (e.target.classList[1] === 'note-playing') {
 		e.target.classList.remove('note-playing');
 
 	} else {
 		e.target.classList.remove('chord-playing');
 	}
-} 
-
+}
 
 window.addEventListener('keypress', makeSound);
 window.addEventListener('keypress', moveButton);
@@ -62,14 +55,14 @@ window.addEventListener('keypress', moveButton);
 const noteKeys = document.querySelectorAll('.note')
 const chordKeys = document.querySelectorAll('.chord')
 
-noteKeys.forEach( note => note.addEventListener('transitionend', removeTransition) )
-chordKeys.forEach( chord => chord.addEventListener('transitionend', removeTransition) )
+noteKeys.forEach(note => note.addEventListener('transitionend', removeTransition))
+chordKeys.forEach(chord => chord.addEventListener('transitionend', removeTransition))
 
 // Event listeners para touch devices
 
-noteKeys.forEach( note => note.addEventListener('touchstart', makeSound) )
-noteKeys.forEach( note => note.addEventListener('touchstart', moveButton) )
+noteKeys.forEach(note => note.addEventListener('touchstart', makeSound))
+noteKeys.forEach(note => note.addEventListener('touchstart', moveButton))
 
-chordKeys.forEach( chord => chord.addEventListener('touchstart', makeSound) )
-chordKeys.forEach( chord => chord.addEventListener('touchstart', moveButton) )
+chordKeys.forEach(chord => chord.addEventListener('touchstart', makeSound))
+chordKeys.forEach(chord => chord.addEventListener('touchstart', moveButton))
 
