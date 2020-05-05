c1cd84aa1ad444b261e1a302038e56d28c733db3
le-santos <lequinterio@gmail.com>
2020-05-05 15:00:43 -0300

    setup de touch events

--- script.js
+++ script.js
@@ -2,24 +2,36 @@
 
 /* TO DO:
 		+ Setup de event para mudar cores do background 
-    	+ TEstar pagina em mobile - fuciona com touch? 
+		+ Tornar responsive para web mobile 
 */
 
 
 const makeSound = function(e) {
-    
-    const sound = document.querySelector(`audio[data-key="${e.key}"]`);
+	console.log(e);
+	if (e.type === 'keypress'){
+		let sound = document.querySelector(`audio[data-key="${e.key}"]`);
+		if(!sound) return;
+		sound.currentTime = 0;
+		sound.play();
 
-    const key = document.querySelector(`button[data-key="${e.key}"]`);
-
-    if(!sound) return;
-    sound.currentTime = 0;
-    sound.play();
+	} else if (e.type === 'touchstart' || e.type === 'touchmove'  ) {
+		let sound = document.querySelector(`audio[data-key="${e.originalTarget.dataset.key}"]`);
+		if(!sound) return;
+		sound.currentTime = 0;
+		sound.play();
+	}
 
 }
 
 const moveButton = function(e) {
-	const key = document.querySelector(`button[data-key="${e.key}"]`);
+	let key
+	console.log(e.type)
+
+	if (e.type === "keypress" ){
+		key = document.querySelector(`button[data-key="${e.key}"]`);
+	} else if (e.type === "touchstart"){
+		key = document.querySelector(`button[data-key="${e.originalTarget.dataset.key}"]`);
+	}
 
 	if (key.className === 'chord chord-playing' || key.className === 'note note-playing'){
 		return
@@ -44,14 +56,20 @@ const removeTransition = function(e) {
 } 
 
 
+window.addEventListener('keypress', makeSound);
+window.addEventListener('keypress', moveButton);
 
 const noteKeys = document.querySelectorAll('.note')
 const chordKeys = document.querySelectorAll('.chord')
 
+noteKeys.forEach( note => note.addEventListener('transitionend', removeTransition) )
+chordKeys.forEach( chord => chord.addEventListener('transitionend', removeTransition) )
 
-noteKeys.forEach( note => note.addEventListener('transitionend', removeTransition)	)
-chordKeys.forEach( chord => chord.addEventListener('transitionend', removeTransition)	)
+// Event listeners para touch devices
 
-window.addEventListener('keypress', makeSound);
-window.addEventListener('keypress', moveButton);
+noteKeys.forEach( note => note.addEventListener('touchstart', makeSound) )
+noteKeys.forEach( note => note.addEventListener('touchstart', moveButton) )
+
+chordKeys.forEach( chord => chord.addEventListener('touchstart', makeSound) )
+chordKeys.forEach( chord => chord.addEventListener('touchstart', moveButton) )
 
