46961f1ed0c62e1c58e7edb8ea60f3991b63bca4
le-santos <lequinterio@gmail.com>
2020-08-04 19:35:46 -0300

    Changed spacings, add some comments

--- script.js
+++ script.js
@@ -1,68 +1,77 @@
-"use strict"
-
 const makeSound = function (e) {
-	console.log(e);
-	if (e.type === 'keypress') {
-		let sound = document.querySelector(`audio[data-key="${e.key}"]`);
-		if (!sound) return;
-		sound.currentTime = 0;
-		sound.play();
-
-	} else if (e.type === 'touchstart') {
-		let sound = document.querySelector(`audio[data-key="${e.originalTarget.dataset.key}"]`);
-		if (!sound) return;
-		sound.currentTime = 0;
-		sound.play();
-	}
-
-}
+  if (e.type === "keypress") {
+    let sound = document.querySelector(`audio[data-key="${e.key}"]`);
+    if (!sound) return;
+    sound.currentTime = 0;
+    sound.play();
+  } else if (e.type === "touchstart") {
+    let sound = document.querySelector(
+      `audio[data-key="${e.originalTarget.dataset.key}"]`
+    );
+    if (!sound) return;
+    sound.currentTime = 0;
+    sound.play();
+  }
+};
 
 const moveButton = function (e) {
-	let key
-	console.log(e.type)
+  let key;
+  //check event type (keyboard or touch)
+  if (e.type === "keypress") {
+    key = document.querySelector(`button[data-key="${e.key}"]`);
+  } else if (e.type === "touchstart") {
+    key = document.querySelector(
+      `button[data-key="${e.originalTarget.dataset.key}"]`
+    );
+  }
 
-	if (e.type === "keypress") {
-		key = document.querySelector(`button[data-key="${e.key}"]`);
-	} else if (e.type === "touchstart") {
-		key = document.querySelector(`button[data-key="${e.originalTarget.dataset.key}"]`);
-	}
+  // avoid multiple triggering
+  if (
+    key.className === "chord chord-playing" ||
+    key.className === "note note-playing"
+  ) {
+    return;
+    // add classlists to the movement effect
+  } else if (key.className === "chord") {
+    key.classList.add("chord-playing");
+  } else {
+    key.classList.add("note-playing");
+  }
+};
 
-	if (key.className === 'chord chord-playing' || key.className === 'note note-playing') {
-		return
-	} else if (key.className === 'chord') {
-		key.classList.add('chord-playing');
-	} else {
-		key.classList.add('note-playing');
-	}
-}
-
+// remove classes when transform triggers transitionend event
 const removeTransition = function (e) {
+  if (e.propertyName !== "transform") {
+    return;
+  } else if (e.target.classList[1] === "note-playing") {
+    e.target.classList.remove("note-playing");
+  } else {
+    e.target.classList.remove("chord-playing");
+  }
+};
 
-	if (e.propertyName !== 'transform') {
-		return;
+// Adding listeners
+window.addEventListener("keypress", makeSound);
+window.addEventListener("keypress", moveButton);
 
-	} else if (e.target.classList[1] === 'note-playing') {
-		e.target.classList.remove('note-playing');
+const noteKeys = document.querySelectorAll(".note");
+const chordKeys = document.querySelectorAll(".chord");
 
-	} else {
-		e.target.classList.remove('chord-playing');
-	}
-}
-
-window.addEventListener('keypress', makeSound);
-window.addEventListener('keypress', moveButton);
-
-const noteKeys = document.querySelectorAll('.note')
-const chordKeys = document.querySelectorAll('.chord')
-
-noteKeys.forEach(note => note.addEventListener('transitionend', removeTransition))
-chordKeys.forEach(chord => chord.addEventListener('transitionend', removeTransition))
+noteKeys.forEach((note) =>
+  note.addEventListener("transitionend", removeTransition)
+);
+chordKeys.forEach((chord) =>
+  chord.addEventListener("transitionend", removeTransition)
+);
 
 // Event listeners para touch devices
 
-noteKeys.forEach(note => note.addEventListener('touchstart', makeSound))
-noteKeys.forEach(note => note.addEventListener('touchstart', moveButton))
+noteKeys.forEach((note) => note.addEventListener("touchstart", makeSound));
+noteKeys.forEach((note) => note.addEventListener("touchstart", moveButton));
+chordKeys.forEach((chord) => chord.addEventListener("touchstart", makeSound));
+chordKeys.forEach((chord) => chord.addEventListener("touchstart", moveButton));
 
-chordKeys.forEach(chord => chord.addEventListener('touchstart', makeSound))
-chordKeys.forEach(chord => chord.addEventListener('touchstart', moveButton))
-
+/* TO DO:
+		+ Make responsive 
+    + Check Script defer/async => html.index DOM seems to load only after page refresh, on a second load. 
+*/
